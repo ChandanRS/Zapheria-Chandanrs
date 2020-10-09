a8712eb13a8ac84e8584f8a00d938b5198396718
Chandan Shetti <chandanshetti06@gmail.com>
2020-10-09 15:47:56 +0530

    Corrected mobile view page1

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 const navBar = document.getElementById("navbar")
 console.log(window.pageXOffset , window.pageYOffset,navBar.offsetTop);
 
-console.log(navBar)
 if(window.pageYOffset <= navBar.offsetTop){
     navBar.style.backgroundColor = 'transparent'
 }
@@ -12,7 +11,7 @@ else if(window.pageYOffset >= navBar.offsetTop){
     navBar.style.backgroundColor = 'rgba(0,0,0,0.2'
 }
 
-
+//COROUSAL
 var slideIndex = 0;
 showSlides();
 
@@ -30,5 +29,19 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "flex";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 4000); // Change image every 2 seconds
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
+
+
+// TOGGLE NAVBAR
+const toggler = document.getElementById("toggle")
+const menu = document.getElementById("nativagtionItems")
+console.log(toggler)
+menu.style.display='none'
+toggler.addEventListener('click',function(){
+  if( menu.style.display == 'none'){
+    menu.style.display = 'flex'
+  }
+  else
+  menu.style.display='none'
+})
